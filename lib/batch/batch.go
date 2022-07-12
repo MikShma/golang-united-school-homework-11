01d279ef20e6bed6e519036c4d9c298be81013f3
Mikita Shmatko <mikita.shmatko@syngenta.com>
2022-07-12 22:37:42 +0400

    solution v1

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,24 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, 0, n)
+	wg := new(sync.WaitGroup)
+	mu := new(sync.Mutex)
+
+	for k := int64(0); k < n; k += pool {
+
+		wg.Add(int(pool))
+		for i := int64(0); i < pool; i++ {
+			go func(wg *sync.WaitGroup, mu *sync.Mutex, i int64) {
+				defer wg.Done()
+				user := getOne(k + i)
+				mu.Lock()
+				res = append(res, user)
+				mu.Unlock()
+			}(wg, mu, i)
+		}
+		wg.Wait()
+	}
+
+	return res
 }
